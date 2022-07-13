bb106d1b3bcb9cbe5eb611e150fef3816e223f14
Amit Jangid <amitjangid1999@gmail.com>
2022-07-13 12:29:15 +0530

    code-fromating

--- weather-frontend/src/App.js
+++ weather-frontend/src/App.js
@@ -1,10 +1,7 @@
 import Header from "./components/header";
 
 function App() {
-  return ( 
-    <Header/>
-    
-     );
+  return <Header />;
 }
 
 export default App;
--- weather-frontend/src/components/card.js
+++ weather-frontend/src/components/card.js
@@ -1,16 +1,23 @@
-
-const Card = ({data}) => {
-    return (
-        <div style={{display: 'flex', justifyContent:'center', alignItems: 'center', flexDirection: 'column', marginTop: '10px',color:"white"}}>
-            <p>City Name : {data[0]?.lname}</p>
-            <p>Country Name : {data[0]?.cname}</p>
-            <p>Temperature : {data[0]?.tname}</p>
-            <p>Humidity : {data[0]?.humidity}</p>
-            <p>Region Name : {data[0]?.lregion}</p>
-            <p>Continent Name : {data[0]?.continent}</p>
-
-        </div>
-    )
-}
+const Card = ({ data }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        marginTop: "10px",
+        color: "white",
+      }}
+    >
+      <p>City Name : {data[0]?.lname}</p>
+      <p>Country Name : {data[0]?.cname}</p>
+      <p>Temperature : {data[0]?.tname}</p>
+      <p>Humidity : {data[0]?.humidity}</p>
+      <p>Region Name : {data[0]?.lregion}</p>
+      <p>Continent Name : {data[0]?.continent}</p>
+    </div>
+  );
+};
 
 export default Card;
